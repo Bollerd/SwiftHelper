43a52370b6ecd35310497bf63a2207db33f97389
Dirk Boller <github@dbweb.info>
2023-01-08 19:36:46 +0100

    Localized Userdefaults Message Enum

--- Sources/SwiftHelperExtensions/Extensions/UserDefaults.swift
+++ Sources/SwiftHelperExtensions/Extensions/UserDefaults.swift
@@ -3,9 +3,9 @@ import Foundation
 //TODO move error text to localization strings
 
 public enum ObjectSavableError: String, LocalizedError {
-    case unableToEncode = "Unable to encode object into data"
-    case noValue = "No data object found for the given key"
-    case unableToDecode = "Unable to decode object into given type"
+    case unableToEncode = she.translateTextForPackage(keyText: "Unable to encode object into data")
+    case noValue = she.translateTextForPackage(keyText: "No data object found for the given key")
+    case unableToDecode = she.translateTextForPackage(keyText: "Unable to decode object into given type")
     
     public var errorDescription: String? {
         rawValue
