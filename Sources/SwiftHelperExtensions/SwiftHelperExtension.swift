24cbee93aa54378a48d402f169a49197d77f8348
Dirk Boller <github@dbweb.info>
2023-12-21 18:04:50 +0100

    Added new JSON encode/decode functions

--- Sources/SwiftHelperExtensions/SwiftHelperExtension.swift
+++ Sources/SwiftHelperExtensions/SwiftHelperExtension.swift
@@ -47,6 +47,53 @@ public struct she {
     public static func testLocalization() -> String {
         return she.translateTextForPackage(keyText: "Test")
     }
+    
+    /// converts the JSON string imported into an (array) or siwft classes/structs receiving the return value
+    /// - Parameter jsonString: data to be decoded as data object
+    /// - Returns: (array) of codable class/swift structure) that is defined as receiving variable
+    public static func convertJSONDataToObject<T: Codable>(data: Data) -> T? {
+        do {
+            let encodedData = try JSONDecoder().decode(T.self, from: data)
+            return encodedData
+        } catch {
+            print("Error decoding JSON into object of type \(T.self)")
+            print(error)
+            return nil
+        }
+    }
+    
+    /// converts the JSON string imported into an (array) or siwft classes/structs receiving the return value
+    /// - Parameter jsonString: data to be decoded as String
+    /// - Returns: (array) of codable class/swift structure) that is defined as receiving variable
+    public static func convertJSONStringToObject<T: Codable>(jsonString: String) -> T? {
+        guard let jsonData = jsonString.data(using: .utf8) else {
+            print("Failed to convert the JSON string to data")
+            return nil
+        }
+        
+        do {
+            let encodedData = try JSONDecoder().decode(T.self, from: jsonData)
+            return encodedData
+        } catch {
+            print("Error decoding JSON into object of type \(T.self)")
+            print(error)
+            return nil
+        }
+    }
+    
+    /// Convert any Swift Codable Object into a String (single object or array of objects)
+    /// - Parameter data: codable Swift class/struct (or array of those)
+    /// - Returns: String of encoded data
+    public static func convertObjectToJSONString<T: Codable>(data: T) -> String? {
+        do {
+            let encoded = try JSONEncoder().encode(data)
+            return encoded
+        } catch {
+            print("Error encoding objects to JSON")
+            print(error)
+            return nil
+        }
+    }
 }
 
 
