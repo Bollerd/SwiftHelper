0b252c89d90c80037fdb583eb59605354c77acfa
Dirk Boller <github@dbweb.info>
2023-12-23 19:06:57 +0100

    bugfixes

--- Sources/SwiftHelperExtensions/Extensions/DispatchQueue.swift
+++ Sources/SwiftHelperExtensions/Extensions/DispatchQueue.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension DispatchQueue {
-    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> String)? = nil) {
+    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> String?)? = nil) {
         DispatchQueue.global(qos: .background).async {
             background?()
             if let completion = completion {
